d4ba220ab334a1a8aa17cf0d0622fba40b2e1ec7
lab596 <86579136+lab596@users.noreply.github.com>
2022-05-31 11:39:46 -0700

    HardCoded Test of HTTP using HTML website

--- src/main/java/com/lab596/httpserver/HttpServer.java
+++ src/main/java/com/lab596/httpserver/HttpServer.java
@@ -12,6 +12,12 @@ package com.lab596.httpserver;
 import com.lab596.httpserver.config.Configuration;
 import com.lab596.httpserver.config.ConfigurationManager;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 //Driver class for the server
 public class HttpServer {
 
@@ -24,6 +30,36 @@ public class HttpServer {
         //testing configuration
         System.out.println("Using Port: " + conf.getPort());
         System.out.println("Using Webroot: " + conf.getWebroot());
+
+        try {
+            //allows connection through port
+            ServerSocket serverSocket = new ServerSocket(conf.getPort());
+            Socket socket = serverSocket.accept();
+
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            //simple html test
+            String html = "<html><head><title>Simple Java HTTP Server </title></head><h1>This page was served using Simple Java HTTP Server</h1><body></body></html>";
+
+            final String CRLF = "\n\r"; //13,10
+
+            //required to read the HTML
+            String response =
+                    "HTTP/11.1 200 OK" + CRLF + //Status Line : HTTP VERSION RESPONSE_CODE RESPONSE_MESSAGE
+                            "Content-Length: " + html.getBytes().length + CRLF +
+                            CRLF +
+                            html +
+                            CRLF + CRLF;
+            //outputting what is read
+            outputStream.write(response.getBytes());
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
